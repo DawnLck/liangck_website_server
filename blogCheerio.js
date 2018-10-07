21942542834b91f213b47b1d7da42b75b4fa26e5
LiangChaokai <youchoudeyu@qq.com>
2018-10-07 20:21:00 +0800

    cherrio Express

---
+++ blogCheerio.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const https = require("https");
+
+const cheerio = require("cheerio");
+
+const wz = "https://segmentfault.com/blog/dawnlck";
+
+module.exports.getBlogs = function () {
+  let strHtml = "";
+  let results = [];
+
+  return new Promise((resolve, reject)=>{
+    https.get(wz, function (res) {
+      res.on("data", function (chunk) {
+        strHtml += chunk;
+      });
+      res.on("end", function () {
+        let $ = cheerio.load(strHtml);
+        $(".stream-list.blog-stream section").each((item, i) => {
+          results.push({
+            title: $(i).find('h2').text() || 'Error',
+            url: `https://segmentfault.com${$(i).find('h2 a').attr('href')}`,
+            time: /[01]?\d月[0-3]?\d日/gi.exec($(i).find('.author').text())[0] || $(i).find('.author').text() || 'Error',
+            abstract: $(i).find('p').text() || 'Error',
+            zan: $(i).find('.stream__item-zan-number').text() || 'Error'
+          })
+        });
+        console.log('Blog Count: ' + results.length);
+        resolve(results);
+      });
+    });
+  });
+
+};
+
--- server.js
+++ server.js
@@ -1,44 +1,23 @@
-//导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
-const Koa = require('koa');
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
+const express = require('express');
+const blogCheerio = require('./blogCheerio').getBlogs;
 
-// 创建一个Koa对象表示web app本身:
-const app = new Koa();
+const app = express();
 
-app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-  await next(); // 调用下一个middleware
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
-app.use(async (ctx, next) => {
-  const start = new Date().getTime(); // 当前时间
-  await next(); // 调用下一个middleware
-  const ms = new Date().getTime() - start; // 耗费时间
-  console.log(`Time: ${ms}ms`); // 打印耗费时间
+app.get('/getBlogs', function (req, res) {
+  blogCheerio().then((result)=>{
+    console.log(`Result Count::${result.length}`);
+    res.send(result);
+  }, err => {
+    console.log(`Err::${err}`);
+  })
 });
 
-// // 对于任何请求，app将调用该异步函数处理请求：
-// app.use(async (ctx, next) => {
-//   await next();
-//   ctx.response.type = 'text/html';
-//   ctx.response.body = '<h1>Hello, koa2!</h1>';
-// });
-
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-  var name = ctx.params.name;
-  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+let server = app.listen(8086, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log(`服务器已经开启，访问地址为http://${host}:${port}`);
 });
-
-router.get('/', async (ctx, next) => {
-  ctx.response.body = '<h1>Index</h1>';
-});
-
-// add router middleware:
-app.use(router.routes());
-
-
-// 在端口3000监听:
-app.listen(3000);
-console.log('app started at port 3000...');
